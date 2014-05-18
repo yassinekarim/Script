b0e113143374136b476d8504a043a758838f28ba
yassinekarim <yassine.karim.104@gmail.com>
2014-05-18 23:29:03 +0200

    minor update

--- script_pom.py
+++ script_pom.py
@@ -9,10 +9,14 @@ if len(sys.argv) < 2:
 tree = ET.parse(sys.argv[1])
 root = tree.getroot()
 dependencies=root.findall("xmlns:dependencies/xmlns:dependency", namespaces={'xmlns': 'http://maven.apache.org/POM/4.0.0'})
+richfaces=false;
 for element in dependencies:
     if isinstance(element.tag, basestring):
         if("org.richfaces." in element.find("xmlns:groupId",namespaces={'xmlns': 'http://maven.apache.org/POM/4.0.0'}).text):
-            element.remove()
+            if(not richfaces):
+                newdep = ET.Element({http://maven.apache.org/POM/4.0.0} + "dependency")
+                element.getparent().append(newdep)
+            element.getparent().remove(element)
         if("com.sun.facelets" in element.find("xmlns:groupId",namespaces={'xmlns': 'http://maven.apache.org/POM/4.0.0'}).text):
-
+            element.remove()
 # tree.write(sys.argv[1],pretty_print=True,encoding='utf-8')
