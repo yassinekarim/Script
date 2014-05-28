3e155432ed143f20d07d89fcf2c057be099802a0
ykarim <yassine.karim@gmail.com>
2014-05-28 17:38:06 +0200

    h:outputStyle

--- migration/richfaces/action/script_a4j.py
+++ migration/richfaces/action/script_a4j.py
@@ -7,6 +7,24 @@ class A4jElement:
     """"upgrade a4j tag"""
     a4jNs="{http://richfaces.org/a4j}"
     hNs="{http://java.sun.com/jsf/html}"
+    def parseSrc(cls,src):
+        listStr=src=split('/')
+        name=listStr.pop() #  remove and return the last elemen of the list
+        last=listStr.pop()
+        library=""
+        for it in listStr:
+            library+=it+"/"
+        library+=last
+        return library,name
+    parseSrc=classmethod(parseSrc)
+    def ressourceUpdate(cls,element):
+        """move the ressources to the right location and upate element"""
+        src=element.get("src")
+        library,name=A4jElement.parseSrc(src)
+        element.attrib.pop("src")
+        element.set("library",library)
+        element.set("name",name)
+    ressourceUpdate=classmethod(ressourceUpdate)
     def componantChange(cls,element):
         """migrate a4j tag"""
         if (element.tag== cls.a4jNs+"actionparam"):
@@ -14,7 +32,7 @@ class A4jElement:
         elif (element.tag== cls.a4jNs+"form"):
             element.tag=cls.hNs+"form"
         elif (element.tag== cls.a4jNs+"region"):
-            print ("selfRendered, renderRegionOnly, ajaxLsitener and immediate attribute are removed/not implemented")
+            print ("selfRendered, renderRegionOnly, ajaxListener and immediate attribute are removed/not implemented")
         elif (element.tag== cls.a4jNs+"AjaxListener"):
             print ("a4j:AjaxListener removed didn't replaced yet")
         elif (element.tag== cls.a4jNs+"support"):
@@ -32,5 +50,11 @@ class A4jElement:
             if(element.get("layout")=="none"):
                 element.attrib.pop(layout)
                 element.set("rendered","false")
+        elif(element.tag== cls.a4jNs+"loadStyle" ):
+            element.tag=hNs+"outputStylesheet"
+            A4jElement.ressourceUpdate(element)
+        elif(element.tag== cls.a4jNs+"loadScript"):
+            element.tag=hNs+"outputScript"
+            A4jElement.ressourceUpdate(element)
         return element
     componantChange=classmethod(componantChange)
